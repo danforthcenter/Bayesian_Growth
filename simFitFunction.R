154e2c3c48e99e0cddbb913e9f000ab04ed5d50e
josh sumner <joshuaquinnsumner@gmail.com>
2021-09-08 10:44:38 -0500

    removing pipes and whatnot

--- simFitFunction.R
+++ simFitFunction.R
@@ -15,10 +15,9 @@ growthSim <- function(x,phi1,phi2,phi3){ # make function to do a bunch of growth
 # define function that makes (however many) iterations of df, using provided parameters.
 
 modelSims<-function(iterations = 10, xTime=25, nSamples = 20, phi1_1=200, phi2_1=13, phi3_1=3, phi1_2=160, phi2_2=13, phi3_2=3.5){
-  #metrics_df<-data.frame(iteration = rep(0, iterations)) #specify output columns
-  startTime<-Sys.time()
+  #loop through iterations making data, model, and gathering results
   for (i in 1:iterations){
-    cat("\nStarting Iteration ", i)
+    cat("\nStarting Iteration ", i, "/",iterations,"\n")
   iteration_row<-data.frame(iteration = i, elpd_loo = NA,elpd_loo_se=NA, p_loo=NA, p_loo_se=NA, looIC=NA,looIC_se=NA) #store iteration number
   x<-1:xTime
   dat <- rbind(
@@ -37,10 +36,7 @@ modelSims<-function(iterations = 10, xTime=25, nSamples = 20, phi1_1=200, phi2_1
                                           "y"=growthSim(x,phi1_2, phi2_2, phi3_2),
                                           stringsAsFactors = F)))
   )
-  #now do things to dat
-  #things I want to do to dat: Fit a model
-                              # Get model metrics
-                              # Store model metrics in a df
+        # model the data
   prior_none <- prior(lognormal(log(130), .25),nlpar = "phi1",coef="treatmenta") +
     prior(lognormal(log(130), .25),nlpar = "phi1",coef="treatmentb") +
     prior(lognormal(log(12), .25), nlpar = "phi2",coef="treatmenta") +
@@ -56,9 +52,9 @@ modelSims<-function(iterations = 10, xTime=25, nSamples = 20, phi1_1=200, phi2_1
                   cores = 2, chains = 2, backend = "cmdstanr", #threads = threading(4),
                   control = list(adapt_delta = 0.999,max_treedepth = 20),
                   inits = function(){list(b_phi1=rgamma(2,1),b_phi2=rgamma(2,1),b_phi3=rgamma(2,1))})
-  #get model metrics
+        #get model metrics
   fit_none<-add_criterion(fit_none, "loo")
-  #store iteration info in dataframe
+        #store iteration info in dataframe row
   iteration_row$elpd_loo[1]<-fit_none$criteria$loo$estimates[1,1]
   iteration_row$elpd_loo_se[1]<-fit_none$criteria$loo$estimates[1,2]
   iteration_row$p_loo[1]<-fit_none$criteria$loo$estimates[2,1]
@@ -70,31 +66,32 @@ modelSims<-function(iterations = 10, xTime=25, nSamples = 20, phi1_1=200, phi2_1
   fixed <- cbind(rownames(fixed), data.frame(fixed, row.names=NULL))
   colnames(fixed)[1]="label"
   fitSumWide<-pivot_wider(fixed, names_from = label, values_from = colnames(fixed)[2:8])
-  fitSumWide<-fitSumWide%>%select(contains("Estimate"), contains("Est.Error"))
+  fitSumWide<-select(fitSumWide, contains("Estimate"), contains("Est.Error"))
   iteration_row<-cbind(iteration_row, fitSumWide)
+        # append rows
   if(i==1){
     metrics_df<-iteration_row
   } else{
-    metrics_df<-rbind(metrics_df, iteration_row)
-  }
-  
+    metrics_df<-rbind(metrics_df, iteration_row)}
   }#close iterations for loop
   return(metrics_df)
 }
 
 start<-Sys.time()
 metrics_df<-modelSims()
-metrics_df%>%summarize(across(.cols=everything(), .fns = list(mean, sd), .names = "{.fn}_{.col}"))
 Sys.time()-start
 
+summ<-summarize(metrics_df, across(.cols=everything(), .fns = list(mean, sd), .names = "{.col}_{.fn}"))
+colnames(summ)<-str_replace_all(str_replace_all(colnames(summ), "_1", "_mean"), "_2", "_sd")
+View(summ)
 
-
-
-
-
-
-
-
+r1<-select(summ, contains("_mean"))
+colnames(r1)<-str_remove_all(colnames(r1), "_mean")
+r2<- select(summ, contains("_sd"))
+colnames(r2)<-str_remove_all(colnames(r2), "_sd")
+summ2<-rbind(r1, r2)
+row.names(summ2)<-c("mean", "sd")
+View(summ2)
 
 
 
